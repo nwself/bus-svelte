79c28a6a7af49d0d12ed90422bf21f9d3bb98cb9
Nathan Self <nwself@cs.vt.edu>
2023-08-09 22:22:14 -0400

    Absolutely position svg

--- src/lib/components/SVG.svelte
+++ src/lib/components/SVG.svelte
@@ -1,5 +1,15 @@
-<svg width="1000" height="1000" viewBox="-250 -250 500 500" xmlns="http://www.w3.org/2000/svg">
+<svg width="1000" height="1000" viewBox="-300 -300 600 600" xmlns="http://www.w3.org/2000/svg">
 
-  <rect x="-250" y="-250"width="1000" height="1000" fill="#b4cb6b"/>
+  <rect x="-300" y="-300" width="1000" height="1000" fill="#b4cb6b"/>
   <slot></slot>
 </svg>
+
+<style>
+  svg {
+    position: absolute;
+    top: 45px;
+    width: 100vw;
+    height: 100vw;
+    z-index: -1;
+  }
+</style>
